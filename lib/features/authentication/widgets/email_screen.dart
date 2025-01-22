4f60e31fc9b8d39ca7543d1951a2cced7f246dce
sauvi9non <xxxdmstj@gmail.com>
2025-01-22 16:00:43 +0900

    [FEAT] email 유효성검사, 키보드 관련 설정, 다음 비밀번호 설정으로 넘어가기

--- lib/features/authentication/widgets/email_screen.dart
+++ lib/features/authentication/widgets/email_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/features/authentication/password_screen.dart';
 
 import '../../../constants/sizes.dart';
 import 'form_button.dart';
@@ -12,73 +13,109 @@ class EmailScreen extends StatefulWidget {
 }
 
 class _EmailScreenState extends State<EmailScreen> {
-  final TextEditingController _usernameController = TextEditingController();
-  String _username = ""; //
+  final TextEditingController _emailController = TextEditingController();
+  String _email = ""; //
 
   @override
   void initState() {
     super.initState();
-    _usernameController.addListener(() {
+    _emailController.addListener(() {
       setState(() {
-        _username = _usernameController.text;
+        _email = _emailController.text;
       });
     });
   }
 
   @override
   void dispose() {
-    _usernameController.dispose();
+    _emailController.dispose();
     super.dispose(); //마지막에 아무래도 얘가 부모그거니까...
+  }
+
+  String? _isEmailValid() {
+    if (_email.isEmpty) return null;
+    final regExp = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    if (!regExp.hasMatch(_email)) {
+      return "Not Valid Email";
+    }
+    return null;
+  }
+
+  void _onScaffoldTap() {
+    //유저가 스캐폴드의 아무곳이나 누르면 email screen이 focus를 잃는다
+    FocusScope.of(context).unfocus();
+  }
+
+  void _onSubmit() {
+    if (_isEmailValid() != null || _email.isEmpty) return;
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PasswordScreen(),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        foregroundColor: Colors.black,
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: Text(
-          "Sign up",
-          style: TextStyle(
-            fontSize: Sizes.size16 + Sizes.size2,
-            fontWeight: FontWeight.w600,
+    return GestureDetector(
+      onTap: () => _onScaffoldTap(),
+      child: Scaffold(
+        appBar: AppBar(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Text(
+            "Sign up",
+            style: TextStyle(
+              fontSize: Sizes.size16 + Sizes.size2,
+              fontWeight: FontWeight.w600,
+            ),
           ),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Gaps.v40,
-            Text(
-              "What's your email?",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: Sizes.size24,
-                fontWeight: FontWeight.w700,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gaps.v40,
+              Text(
+                "What's your email?",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
-            ),
-            Gaps.v16,
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                  hintText: "Email", //placeholder
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.grey.shade400,
-                    ),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
+              Gaps.v16,
+              TextField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                autocorrect: false,
+                onEditingComplete: _onSubmit,
+                decoration: InputDecoration(
+                    errorText: _isEmailValid(),
+                    hintText: "Email", //placeholder
+                    enabledBorder: UnderlineInputBorder(
                       borderSide: BorderSide(
-                    color: Colors.grey.shade400,
-                  ))),
-              cursorColor: Theme.of(context).primaryColor, //커서 색깔
-            ),
-            Gaps.v16,
-            FormButton(disabled: _username.isEmpty),
-          ],
+                        color: Colors.grey.shade400,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ))),
+                cursorColor: Theme.of(context).primaryColor, //커서 색깔
+              ),
+              Gaps.v16,
+              GestureDetector(
+                onTap: _onSubmit,
+                child: FormButton(
+                    disabled: _email.isEmpty || _isEmailValid() != null),
+              ), //이거 색만 바뀌는거 아닌가
+            ],
+          ),
         ),
       ),
     );

3c12feda20f598d022e15f0df63864d286d0ac49
sauvi9non <xxxdmstj@gmail.com>
2025-01-22 15:25:12 +0900

    [REFACTOR]

---
+++ lib/features/authentication/widgets/form_button.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../../../constants/sizes.dart';
+
+class FormButton extends StatelessWidget {
+  // const FormButton({
+  //   super.key,
+  //   required String username, //여기 username은 부모에게서 받은
+  // }) : _username = username; // _username은 여기서 쓸거
+
+  // final String _username; //그런데 어차피 FormButton에서는 username의 유무만 궁굼한거니까.
+
+  const FormButton({
+    super.key,
+    required this.disabled,
+  });
+
+  final bool disabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        padding: EdgeInsets.symmetric(vertical: Sizes.size16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(Sizes.size10),
+          color:
+              disabled ? Colors.grey.shade300 : Theme.of(context).primaryColor,
+        ),
+        child: AnimatedDefaultTextStyle(
+          style: TextStyle(
+            color: disabled ? Colors.grey.shade400 : Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+          duration: Duration(milliseconds: 500),
+          child: Text(
+            "Next",
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/authentication/widgets/username_screen.dart
+++ lib/features/authentication/widgets/username_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 
 import '../../../constants/sizes.dart';
+import 'form_button.dart';
 
 class UsernameScreen extends StatefulWidget {
   const UsernameScreen({super.key});
@@ -22,6 +23,12 @@ class _UsernameScreenState extends State<UsernameScreen> {
         _username = _usernameController.text;
       });
     });
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    super.dispose(); //마지막에 아무래도 얘가 부모그거니까...
   }
 
   @override
@@ -81,49 +88,6 @@ class _UsernameScreenState extends State<UsernameScreen> {
             Gaps.v16,
             FormButton(disabled: _username.isEmpty),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class FormButton extends StatelessWidget {
-  // const FormButton({
-  //   super.key,
-  //   required String username, //여기 username은 부모에게서 받은
-  // }) : _username = username; // _username은 여기서 쓸거
-
-  // final String _username; //그런데 어차피 FormButton에서는 username의 유무만 궁굼한거니까.
-
-  const FormButton({
-    super.key,
-    required this.disabled,
-  });
-
-  final bool disabled;
-
-  @override
-  Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: 1,
-      child: AnimatedContainer(
-        duration: Duration(milliseconds: 300),
-        padding: EdgeInsets.symmetric(vertical: Sizes.size16),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(Sizes.size10),
-          color:
-              disabled ? Colors.grey.shade300 : Theme.of(context).primaryColor,
-        ),
-        child: AnimatedDefaultTextStyle(
-          style: TextStyle(
-            color: disabled ? Colors.grey.shade400 : Colors.white,
-            fontWeight: FontWeight.w600,
-          ),
-          duration: Duration(milliseconds: 500),
-          child: Text(
-            "Next",
-            textAlign: TextAlign.center,
-          ),
         ),
       ),
     );

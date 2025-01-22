af4c0b4480e2b80d2c38454bef5d081a1b6eea8c
sauvi9non <xxxdmstj@gmail.com>
2025-01-22 15:19:06 +0900

    [REFACT] formButton 리팩토링

--- lib/features/authentication/widgets/username_screen.dart
+++ lib/features/authentication/widgets/username_screen.dart
@@ -12,7 +12,7 @@ class UsernameScreen extends StatefulWidget {
 
 class _UsernameScreenState extends State<UsernameScreen> {
   final TextEditingController _usernameController = TextEditingController();
-  String _username = "";
+  String _username = ""; //
 
   @override
   void initState() {
@@ -79,28 +79,51 @@ class _UsernameScreenState extends State<UsernameScreen> {
               cursorColor: Theme.of(context).primaryColor, //커서 색깔
             ),
             Gaps.v16,
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: AnimatedContainer(
-                duration: Duration(milliseconds: 300),
-                padding: EdgeInsets.symmetric(vertical: Sizes.size16),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(Sizes.size10),
-                  color: _username.isEmpty
-                      ? Colors.grey
-                      : Theme.of(context).primaryColor,
-                ),
-                child: Text(
-                  "Next",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-            ),
+            FormButton(disabled: _username.isEmpty),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class FormButton extends StatelessWidget {
+  // const FormButton({
+  //   super.key,
+  //   required String username, //여기 username은 부모에게서 받은
+  // }) : _username = username; // _username은 여기서 쓸거
+
+  // final String _username; //그런데 어차피 FormButton에서는 username의 유무만 궁굼한거니까.
+
+  const FormButton({
+    super.key,
+    required this.disabled,
+  });
+
+  final bool disabled;
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        padding: EdgeInsets.symmetric(vertical: Sizes.size16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(Sizes.size10),
+          color:
+              disabled ? Colors.grey.shade300 : Theme.of(context).primaryColor,
+        ),
+        child: AnimatedDefaultTextStyle(
+          style: TextStyle(
+            color: disabled ? Colors.grey.shade400 : Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+          duration: Duration(milliseconds: 500),
+          child: Text(
+            "Next",
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );

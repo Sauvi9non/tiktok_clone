b2b58ca1180cd4402fd9c246292820c9b1a06f76
sauvi9non <xxxdmstj@gmail.com>
2025-01-22 15:36:16 +0900

    [FEAT] username에서 emailscreen으로 넘어가기

--- lib/features/authentication/widgets/email_screen.dart
+++ lib/features/authentication/widgets/email_screen.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
-import 'package:tiktok_clone/features/authentication/widgets/email_screen.dart';
 
 import '../../../constants/sizes.dart';
 import 'form_button.dart';
 
-class UsernameScreen extends StatefulWidget {
-  const UsernameScreen({super.key});
+class EmailScreen extends StatefulWidget {
+  const EmailScreen({super.key});
 
   @override
-  State<UsernameScreen> createState() => _UsernameScreenState();
+  State<EmailScreen> createState() => _EmailScreenState();
 }
 
-class _UsernameScreenState extends State<UsernameScreen> {
+class _EmailScreenState extends State<EmailScreen> {
   final TextEditingController _usernameController = TextEditingController();
   String _username = ""; //
 
@@ -30,20 +29,6 @@ class _UsernameScreenState extends State<UsernameScreen> {
   void dispose() {
     _usernameController.dispose();
     super.dispose(); //마지막에 아무래도 얘가 부모그거니까...
-  }
-
-  void _onNextTap() {
-    if (_username.isEmpty) {
-      return;
-    } else {
-      Navigator.of(context).push(
-        //왜 여기서는 매개변수로 BuildContext context를 안받아도 되나?
-        MaterialPageRoute(
-          //왜냐면 StatefulWidget안에 있다면 어디서든 context를 사용할 수 있기 때문
-          builder: (context) => const EmailScreen(),
-        ),
-      );
-    }
   }
 
   @override
@@ -68,27 +53,18 @@ class _UsernameScreenState extends State<UsernameScreen> {
           children: [
             Gaps.v40,
             Text(
-              "Create username",
+              "What's your email?",
               style: TextStyle(
                 color: Colors.black,
                 fontSize: Sizes.size24,
                 fontWeight: FontWeight.w700,
               ),
             ),
-            Gaps.v8,
-            Text(
-              "You can always change this laster",
-              style: TextStyle(
-                color: Colors.black54,
-                fontSize: Sizes.size16,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
             Gaps.v16,
             TextField(
               controller: _usernameController,
               decoration: InputDecoration(
-                  hintText: "Username", //placeholder
+                  hintText: "Email", //placeholder
                   enabledBorder: UnderlineInputBorder(
                     borderSide: BorderSide(
                       color: Colors.grey.shade400,
@@ -101,9 +77,7 @@ class _UsernameScreenState extends State<UsernameScreen> {
               cursorColor: Theme.of(context).primaryColor, //커서 색깔
             ),
             Gaps.v16,
-            GestureDetector(
-                onTap: _onNextTap,
-                child: FormButton(disabled: _username.isEmpty)),
+            FormButton(disabled: _username.isEmpty),
           ],
         ),
       ),

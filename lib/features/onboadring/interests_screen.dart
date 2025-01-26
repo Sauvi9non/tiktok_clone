da471e99f0b99d877cb778feb798cdbc36143d3a
sauvi9non <xxxdmstj@gmail.com>
2025-01-27 00:34:33 +0900

    [FEAT] interest chip
    todo: 눌렀을 때 효과 적용이 스크롤바 한번 올렸다 내렸다 해야 된다.

--- lib/features/onboadring/interests_screen.dart
+++ lib/features/onboadring/interests_screen.dart
@@ -1,10 +1,124 @@
 import 'package:flutter/material.dart';
+import '../../constants/gaps.dart';
+import '../../constants/sizes.dart';
+import '../onboadring/interests.dart';
+import 'widgets/interest_chip.dart';
 
-class InterestsScreen extends StatelessWidget {
+class InterestsScreen extends StatefulWidget {
   const InterestsScreen({super.key});
 
   @override
+  State<InterestsScreen> createState() => _InterestsScreenState();
+}
+
+class _InterestsScreenState extends State<InterestsScreen> {
+  final ScrollController _scrollController = ScrollController();
+
+  bool _showTitle = false;
+
+  void _onScroll() {
+    if (_scrollController.offset > 120) {
+      if (_showTitle) return; //setState 계속 호출되는 걸 방지하기 위해
+      setState(() {
+        _showTitle = true;
+      });
+    } else {
+      if (!_showTitle) return;
+      setState(() {
+        _showTitle = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: AnimatedOpacity(
+          opacity: _showTitle ? 1 : 0,
+          duration: Duration(milliseconds: 300),
+          child: Text(
+            "Choose your interests",
+            style: TextStyle(),
+          ),
+        ),
+      ),
+      body: Scrollbar(
+        controller: _scrollController,
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: Sizes.size24,
+              right: Sizes.size24,
+              bottom: Sizes.size16,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Gaps.v32,
+                Text(
+                  "Choose your interests",
+                  style: TextStyle(
+                    fontSize: Sizes.size40,
+                    fontWeight: FontWeight.bold,
+                    height: 1,
+                  ),
+                ),
+                Gaps.v20,
+                Text(
+                  "Get better video recommendations",
+                  style: TextStyle(
+                    fontSize: Sizes.size16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Gaps.v60,
+                Wrap(
+                  runSpacing: 15,
+                  spacing: 20,
+                  children: [
+                    for (var interest in Interests.interests)
+                      InterestChip(interest: interest),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        height: 100,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: Sizes.size10),
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: Sizes.size12),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+            ),
+            child: Text("Next",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: Sizes.size20,
+                )),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/onboadring/widgets/interest_chip.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/features/onboadring/interests.dart';
+
+import '../../../constants/sizes.dart';
+
+class InterestChip extends StatefulWidget {
+  const InterestChip({
+    super.key,
+    required this.interest,
+  });
+
+  final String interest;
+
+  @override
+  State<InterestChip> createState() => _InterestChipState();
+}
+
+class _InterestChipState extends State<InterestChip> {
+  bool _isSelected = false;
+
+  void _onTap() {
+    _isSelected = !_isSelected;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _onTap,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 100),
+        padding: EdgeInsets.symmetric(
+          vertical: Sizes.size12,
+          horizontal: Sizes.size20,
+        ),
+        decoration: BoxDecoration(
+            color: _isSelected ? Theme.of(context).primaryColor : Colors.white,
+            borderRadius: BorderRadius.circular(
+              Sizes.size32,
+            ),
+            border: Border.all(
+              color: Colors.black.withAlpha(10),
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withAlpha(50),
+                blurRadius: 5,
+                spreadRadius: 5,
+              ),
+            ]),
+        child: Text(
+          widget.interest,
+          style: TextStyle(
+            //  fontSize: Sizes.size40,
+            fontWeight: FontWeight.bold,
+            color: _isSelected ? Colors.white : Colors.black87,
+          ),
+        ),
+      ),
+    );
+  }
+}

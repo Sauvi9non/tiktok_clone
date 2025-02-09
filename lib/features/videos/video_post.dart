f62ed728fc99fe3486ed2bdc210ac656eae89419
sauvi9non <xxxdmstj@gmail.com>
2025-02-09 20:38:09 +0900

    [FEAT] 재생, 일시정지 커스텀 애니메이션

--- lib/features/videos/video_post.dart
+++ lib/features/videos/video_post.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
+
+import '../../constants/sizes.dart';
 
 class VideoPost extends StatefulWidget {
   final Function
@@ -13,9 +16,15 @@ class VideoPost extends StatefulWidget {
   State<VideoPost> createState() => _VideoPostState();
 }
 
-class _VideoPostState extends State<VideoPost> {
+class _VideoPostState extends State<VideoPost>
+    with SingleTickerProviderStateMixin {
+  final Duration _animatedDuration = Duration(milliseconds: 150);
+  bool _isPaused = false;
+
   final VideoPlayerController _videoPlayerController =
       VideoPlayerController.asset("assets/videos/video.mp4");
+
+  late final AnimationController _animationController;
 
   void _onVideoChange() {
     if (_videoPlayerController.value.isInitialized) {
@@ -39,6 +48,14 @@ class _VideoPostState extends State<VideoPost> {
   void initState() {
     super.initState();
     _initVideoPlayer();
+    _animationController = AnimationController(
+      vsync: this,
+      lowerBound:
+          1.0, //일반적으로 애니메이션은 lowerBound에서 시작 물론 upperBound에서 시작하게 할 수 도 있다.
+      upperBound: 1.5,
+      value: 1.5, //기본적
+      duration: _animatedDuration,
+    );
   }
 
   @override
@@ -51,6 +68,19 @@ class _VideoPostState extends State<VideoPost> {
     if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
       _videoPlayerController.play();
     }
+  }
+
+  void _onTogglePause() {
+    if (_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.pause();
+      _animationController.reverse();
+    } else {
+      _videoPlayerController.play();
+      _animationController.forward();
+    }
+    setState(() {
+      _isPaused = !_isPaused;
+    });
   }
 
   @override
@@ -66,6 +96,36 @@ class _VideoPostState extends State<VideoPost> {
                 : Container(
                     color: Colors.black,
                   ),
+          ),
+          Positioned.fill(
+            child: GestureDetector(
+              onTap: _onTogglePause,
+            ),
+          ),
+          Positioned.fill(
+            child: IgnorePointer(
+              child: Center(
+                child: AnimatedBuilder(
+                  animation: _animationController,
+                  builder: (context, child) {
+                    //animationController 값 변할때마다 실행된다.
+                    return Transform.scale(
+                      scale: _animationController.value,
+                      child: child, // AnimatedOpacity 넘겨주기 위함
+                    );
+                  },
+                  child: AnimatedOpacity(
+                    opacity: _isPaused ? 1 : 0,
+                    duration: _animatedDuration,
+                    child: FaIcon(
+                      FontAwesomeIcons.circlePlay,
+                      color: Colors.white,
+                      size: Sizes.size96,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),

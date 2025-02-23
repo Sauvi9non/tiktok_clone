2bb198be9e449169f927afa4d62244013db6916e
sauvi9non <xxxdmstj@gmail.com>
2025-02-23 17:16:13 +0900

    viewmodel

--- lib/features/videos/view_modles/playback_config_vm.dart
+++ lib/features/videos/view_modles/playback_config_vm.dart
@@ -1,3 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/features/videos/models/playback_config_model.dart';
+import 'package:tiktok_clone/features/videos/repos/video_playback_config_repo.dart';
 
-class PlaybackConfigViewModel extends ChangeNotifier {}
+class PlaybackConfigViewModel extends ChangeNotifier {
+  final VideoPlaybackConfigRepo _repository;
+  late final PlaybackConfigModel _model = PlaybackConfigModel(
+    muted: _repository.isMuted(),
+    autoplay: _repository.isAutoplay(),
+  );
+
+  PlaybackConfigViewModel(this._repository);
+
+  bool get muted => _model.muted;
+  bool get autoplay => _model.autoplay;
+
+  void setMuted(bool value) {
+    //view에게 이거 expose해주기
+    // 1.repo에서 디스크에 persist하게 저장
+    _repository.setMuted(value);
+    // 2. model (즉 데이터) 수정
+    _model.muted = value;
+    //3 notify하고 있는 모두에게 알리기
+    notifyListeners();
+  }
+
+  void setAutoplay(bool value) {
+    _repository.setAutoplay(value);
+    _model.autoplay = value;
+    notifyListeners();
+  }
+}
